d6ed1858bea28b82ff30b33eb3ded3ffeeff7dbc
Grzegorz Bielski <pesiok@gmail.com>
2024-09-07 16:48:49 +0200

    add readme

---
+++ README.md
@@ -0,0 +1,9 @@
+# ostep-projects
+
+Solution to projects based on the OSTEP course
+
+## Runnings tests
+
+0. Enusure you are on Linux and have gcc and maketools or use provided [dev container](./.devcontainer)
+1. clone https://github.com/remzi-arpacidusseau/ostep-projects
+2. run `make test` in each project folder
---
+++ wgrep/main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// #define MAX_LINE_LENGTH 1000
+
+// void printFile(char *fileName) {
+//     FILE *file = fopen(fileName, "r");
+//     if (file == NULL) {
+//         printf("wcat: cannot open file\n");
+//         exit(1);
+//     }
+
+//     char lineBuffer[MAX_LINE_LENGTH];
+
+//     // fgets() reads a line from the file and stores it in lineBuffer
+//     // fgets() returns NULL when it reaches the end of the file or encounters an error
+//     // fgets() reads at most 999 characters from the file in a single call
+//     // if line is longer than 999 characters, fgets() reads the first 999 characters and appends a null character at the end
+//     while (fgets(lineBuffer, MAX_LINE_LENGTH, file) != NULL) {
+//         printf("%s", lineBuffer);
+//     }
+
+//     if (fclose(file)) {
+//         perror("wcat: cannot close file\n");
+//         exit(1);
+//     }
+// }
+
+// TODO: https://github.com/remzi-arpacidusseau/ostep-projects/tree/master/initial-utilities#wgrep
+
+int main(int argc, char *argv[]) {
+    if (argc <= 1) {
+        // below info is not allowed by test suite
+        // printf("wcat: file1 file2 ...\n");
+        exit(0);
+    }
+
+    for (int i = 1; i < argc; i++) {
+        char *fileName = argv[i];
+        printFile(fileName);
+    }
+
+    return 0;
+}
